c99763620a560a11dcfd778a69d5f89e4ce4d884
rserdah <94141015+rserdah@users.noreply.github.com>
2021-11-11 16:31:44 -0500

    Add files via upload

---
+++ Lab6/InLab/hello.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#hello.sh
+echo "Welcome to computer science society."
+#Ramey Serdah rserdah1@student.gsu.edu
+echo `date`
+ls /home | wc -w
+echo $PATH
+echo $USER
+echo $SHELL
+df
+echo "Please, could you loan me \$25.00"
+echo "if x=2,x*x=4,x/2=1"
+pwd | ls | egrep "^[Cc].*\.sh"
+echo "Goodbye `date "+%H"`"
